f9a22be7bc886beaa6139f36959258bc670aa247
Artsiom Dubinevich <Artsiom_Dubinevich@epam.com>
2020-05-26 16:10:59 +0300

    Add 7.2

--- commertion/user7/2/main.cpp
+++ commertion/user7/2/main.cpp
@@ -2,19 +2,42 @@
 	Имеются контейнеры двух видов: по 130кг и 160кг. 
 	Можно ли полностью загрузить ими грузовик грузоподъёмностью А т.
 */
-
 #include <iostream>
+#include <conio.h>
 
-bool canBeLoad(int containerA, int containerB, int weight)
+#define INT_TO_STBOOL(x) ((x) ? "True" : "False")
+
+bool sollutionExists(int a, int b, int t)
 {
-	if (weight % containerA == 0) return true;
-	if (weight % containerB == 0) return true;
+    bool result = false;
+    for (int i = 0; i <= t / a; i++)
+    {
+        for (int j = 0; j <= t / b; j++)
+        {
+            if (i * a + j * b == t)
+            {
+                result = true;
+            }
+        }
+    }
 
+    return result;
+}
 
-
+void TestCase(int a, int b, int t, int expected)
+{
+    printf("A:[%d], B:[%d], T:[%d], Expected:[%s], Result:[%s]\n",
+        a, b, t, INT_TO_STBOOL(expected), INT_TO_STBOOL(sollutionExists(a, b, t)));
 }
 
 int main()
 {
-	return (0);
+    TestCase(130, 160, 160, true);
+    TestCase(130, 160, 130, true);
+    TestCase(130, 160, 290, true);
+
+    TestCase(130, 160, 280, false);
+    TestCase(130, 160, 270, false);
+
+    return 0;
 }
